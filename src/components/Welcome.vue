ef6a450156a7024e317fdb8110f866dec30f120b
argelis <argelismorenovillegas@gmail.com>
2020-08-15 20:46:29 -0400

    implementando landing

---
+++ src/components/Welcome.vue
@@ -0,0 +1,58 @@
+<template>
+  <section
+    id="welcome"
+    class="overflow-hidden"
+  >
+    <v-row no-gutters>
+      <v-col
+        class="hidden-sm-and-down"
+        md="6"
+      >
+        <v-img
+          :src="require('@/assets/Logotipo_oficial.png')"
+          height="100vh"
+          contain
+        />
+      </v-col>
+
+      <v-col
+        class="align-content-space-between layout wrap"
+        cols="12"
+        md="6"
+      >
+        <base-bubble-1
+          style="transform: rotate(180deg) translateY(25%)"
+        />
+
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="10"
+            md="6"
+          >
+            <base-heading>Nuestra Historia!</base-heading>
+            <base-text>
+              Lorem ipsum dolor sit amet, consectetur ad
+              ipiscin elit. Etiam vulputate augue vel felis gra
+              vida porta. Lorem ipsum dolor sit amet, cons
+              ectetur adipiscing elit.<br>
+              Lorem ipsum dolor sit amet, consectetur ad
+              ipiscin elit. Etiam vulputate augue vel felis gra
+              vida porta. Lorem ipsum dolor sit amet, cons
+              ectetur adipiscing elit.
+            </base-text>
+            <base-btn class="mt-4">
+              Evangelios Diarios
+            </base-btn>
+          </v-col>
+        </v-row>
+
+        <base-bubble-2
+          style="transform: rotate(180deg) translate(-200px, -15%)"
+        />
+      </v-col>
+    </v-row>
+  </section>
+</template>
---
+++ src/components/core/AppBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app-bar
+    v-scroll="onScroll"
+    :color="!isScrolling ? 'transparent' : 'white'"
+    fixed
+    flat
+  >
+    <v-slide-x-transition>
+      <!-- <v-img
+        v-if="showLogo"
+        :src="require('@/assets/logo.png')"
+        class="shrink"
+        contain
+        height="50"
+      /> -->
+      <div v-if="showLogo">
+        <span class="shrink headline red--text text--darken-4">
+          Parroquia San Sebastián de Ocú
+        </span>
+      </div>
+    </v-slide-x-transition>
+
+    <v-spacer />
+
+    <social-media />
+  </v-app-bar>
+</template>
+
+<script>
+  export default {
+    name: 'CoreAppBar',
+
+    components: {
+      SocialMedia: () => import('@/components/SocialMedia'),
+    },
+
+    data: () => ({
+      showLogo: false,
+      isScrolling: false,
+    }),
+
+    methods: {
+      onScroll () {
+        const offset = window.pageYOffset
+        this.isScrolling = offset > 50
+        this.showLogo = offset > 200
+      },
+    },
+  }
+</script>
